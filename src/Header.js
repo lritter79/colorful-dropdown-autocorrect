4460d6982c2e78b1c0cbb33f753d73932094cbb4
Levon Ritter <levong2112@gmail.com>
2021-07-13 22:58:18 -0400

    got the app to compile

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+
+
+function Header() {
+    return (
+      <div className="Header">
+        <input type="text" 
+        class="" 
+        placeholder="Click on this to show the drop down auto complete" 
+        tabindex="1" autocomplete="off" 
+        maxlength="2048">
+
+        </input>
+      </div>
+    );
+  }
+  
+  export default Header;

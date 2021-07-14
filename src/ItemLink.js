b5d11885e9f848ab19325caf8de5d5cc421b96f9
Levon Ritter <levong2112@gmail.com>
2021-07-14 11:13:21 -0400

    added dark text on mouse over

--- src/ItemLink.js
+++ src/ItemLink.js
@@ -2,12 +2,21 @@ import React, { useState } from 'react'
 
 function ItemLink({item, index}) {    
   const [hasMouseOver, setHasMouseOver] = useState(false)
+  const textColorFunction = (color) => {
+    if (color.includes("light")){
+      return "dark"
+    }else {
+      return ""
+    }
+  }
     return (
             <a key={index} 
             onMouseEnter={() => setHasMouseOver(true)}
         onMouseLeave={() => setHasMouseOver(false)}
             style={{backgroundColor: hasMouseOver ? `light${item.color}` : item.color}}  class="tag-link" href="https://bandcamp.com/tag/electronic?from=autocomplete">
-                <div className="item-link">{item.link}</div>
+                <div className={`item-link ${hasMouseOver ? textColorFunction(`light${item.color}`): textColorFunction(item.color)}`}>
+                  {item.link}
+                </div>
                 <div className="shape-container">
                     <span className="arrow"></span>
                 </div>
--- src/Items.js
+++ src/Items.js
@@ -2,9 +2,9 @@ const Items = [
     {"link": "link 1", "color": "blue"}, 
     {"link": "link 2", "color": "green"}, 
     {"link": "link 3", "color": "yellow"}, 
-    {"link": "link 4", "color": "orange"}, 
-    {"link": "link 5", "color": "salmon"}, 
-    {"link": "link 6", "color": "coral"}, 
+    {"link": "link 4", "color": "salmon"}, 
+    {"link": "link 5", "color": "coral"}, 
+    {"link": "link 6", "color": "pink"}, 
     {"link": "link 7", "color": "cyan"}, 
     {"link": "link 8", "color": "grey"}
 ]
